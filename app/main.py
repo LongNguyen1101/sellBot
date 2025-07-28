308f4feca81a7b26b857804896702ff1788784ab
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2025-07-28 09:18:28 +0700

    Add cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from app.api.v1.routes import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.database import Base, engine
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
 app = FastAPI(title="My FastAPI Project")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cho phép tất cả origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Cho phép tất cả các phương thức (GET, POST, PUT, DELETE, ...)
+    allow_headers=["*"],  # Cho phép tất cả các headers
+)
+
 
 # try:
 #     # init_vector_schema_and_indexes()
